56d6d058cf25a12f4d77c70395c42c212bd8514e
henos1206 <henostsegai@gmail.com>
2023-05-03 20:48:11 +0100

    created 'offense' and 'defense' endpoints that display the offensive and defensive stats respectively of every player in the NBA

--- api_utils.py
+++ api_utils.py
@@ -51,3 +51,107 @@ def player_profiles_by_id(ids: list) -> list:
     list_of_ids = [int(id) for id in ids]
     requested_bios = [bio for bio in all_bios if bio['player_id'] in list_of_ids]
     return requested_bios
+
+def create_player_offense_dict(player: pd.DataFrame) -> dict:
+    """Formats a player's offensive stats
+
+    Args:
+        player (pd.DataFrame): all recorded stats for a player
+
+    Returns:
+        dict: formatted stats
+    """
+    return {
+        f"{player['player']}":{
+        'games_played':player['games'],
+        'mins_played':player['mins_played'],
+        'total_points':player['points'],
+        'points_per_game':round(player['points']/player['games'],2),
+        'field_goals':{
+            'field_goal_attempts':player['FGA'],
+            'field_goals':player['FG'],
+            'field_goal_percentage':player['FG%']
+        },
+        '3_pointers':{
+            '3_point_attempts':player['3PA'],
+            '3_point_shots_made':player['3P'],
+            '3_point_percentage':player['3P%']
+        },
+        '2_pointers':{
+            '2_point_attempts':player['2PA'],
+            '2_point_shots_made':player['2P'],
+            '2_point_percentage':player['2P%']
+        },
+        'effective_field_goal_percentage':player['eFG%'],
+        'free_throws':{
+            'free_throw_attempts':player['FTA'],
+            'free_throws_made':player['FT'],
+            'free_throws_percentage':player['FT%']
+        },
+        'assists':{
+            'total_assists':player['AST'],
+            'assists_per_game':round(player['AST']/player['games'],2)
+        }
+    }
+}
+
+def player_offense_stats() -> list:
+    """Creates a list of dictionaries containing player shooting/scoring stats
+
+    Returns:
+        list: list of player stats
+    """
+    df = load_nba_data()
+    player_df = df.filter(regex = "player|games|mins_played|FG|FGA|FG%|3P|3PA|3P%|2P|2PA|2P%|eFG%|FT|FTA|FT%|points|AST")
+    players_bio = [create_player_offense_dict(player) for i,player in player_df.iterrows()]
+    return players_bio
+
+def create_player_defense_dict(player: pd.DataFrame) -> dict:
+    """Formats a player's defensive stats
+
+    Args:
+        player (pd.DataFrame): all recorded stats for a player
+
+    Returns:
+        dict: formatted stats
+    """
+    return {
+        f"{player['player']}":{
+            'games_played':player['games'],
+            'mins_played':player['mins_played'],
+            'rebounding':{
+                'total_rebounds':player['TRB'],
+                'offensive_rebounds':player['ORB'],
+                'offensive_rebounds':round(player['ORB']/player['games'],2),
+                'defensive_rebounds':player['DRB'],
+                'defensive_rebounds_per_game':round(player['DRB']/player['games'],2)
+            },
+            'steals':{
+                'steals':player['STL'],
+                'steals_per_game':round(player['STL']/player['games'],2)
+            },
+            'blocks':{
+                'blocks':player['BLK'],
+                'blocks_per_game':round(player['BLK']/player['games'],2)
+            },
+            'turnovers':{
+                'turnovers':player['TOV'],
+                'turnovers_per_game':round(player['TOV']/player['games'],2)
+            },
+            'fouls':{
+                'fouls':player['fouls'],
+                'fouls_per_game':round(player['fouls']/player['games'],2)
+        }
+    }
+}
+
+def player_defense_stats() -> list:
+    """Creates a list of dictionaries containing player defensive stats
+
+    Returns:
+        list: list of player stats
+    """
+    df = load_nba_data()
+    player_df = df.filter(regex = "player|games|mins_played|ORB|DRB|TRB|STL|BLK|TOV|fouls")
+    players_bio = [create_player_defense_dict(player) for i,player in player_df.iterrows()]
+    return players_bio
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
-from api_utils import player_profile, player_profiles_by_id
+from api_utils import (player_defense_stats, player_offense_stats,
+                       player_profile, player_profiles_by_id)
 
 app = Flask(__name__)
 
@@ -17,6 +18,13 @@ def player_bio_by_id(player_id):
     list_of_ids = player_id.split(',')
     return player_profiles_by_id(list_of_ids)
 
+@app.route('/offense')
+def get_attacking_stats():
+    return player_offense_stats()
+
+@app.route('/defense')
+def get_defensive_stats():
+    return player_defense_stats()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
